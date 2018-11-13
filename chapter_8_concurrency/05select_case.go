876ce3a6a18c0cc7220a458dd0114dd643252465
dongyun.fan <dongyun.fan@mljr.com>
2018-11-13 15:06:33 +0800

    code: select case and nil cases.

--- chapter_8_concurrency/05select_case.go
+++ chapter_8_concurrency/05select_case.go
@@ -1,18 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
-func main() {
+func normal_select(){
 	var c1 = make(chan int, 1)
 	var c2 = make(chan int, 1)
 	var c3 = make(chan int, 1)
-
 	var i1, i2 int
-
 	c1 <- 1
-
 	c3 <- 3
-
 	select {
 	case i1 = <-c1:
 		fmt.Println("received ", i1, " from c1\n")
@@ -27,4 +26,71 @@ func main() {
 	default:
 		fmt.Println("no communication\n")
 	}
+}
+
+func no_buffer_select(){
+	// 注意添加 chan 的 size， 否则是 xchan 就是 nil。
+	var xchan = make(chan int, 1)
+
+		go func() {
+			num := 1
+			for {
+				fmt.Println(num)
+				time.Sleep(1e9)
+
+				num++
+
+				if num == 3 {
+					break
+				}
+			}
+			close(xchan)
+		}()
+
+		select {
+			case <- xchan:  //本行一定要添加，否则出现 all go routines are sleep, deadlock .
+		}
+}
+
+/*
+哪些情形下初始化的 value 是 nil?
+
+// This following line doesn't compile.
+	v := nil
+
+// There must be sufficient information for compiler
+// to deduce the type of a nil value.
+_ = (*struct{})(nil)
+_ = []int(nil)
+_ = map[int]bool(nil)
+_ = chan string(nil)
+_ = (func())(nil)
+_ = interface{}(nil)
+
+// This lines are equivalent to the above lines.
+var _ *struct{} = nil
+var _ []int = nil
+var _ map[int]bool = nil
+var _ chan string = nil
+var _ func() = nil
+var _ interface{} = nil
+
+//
+
+*/
+
+func main() {
+
+	normal_select()
+	no_buffer_select()
+
+
+	_ = (*struct{})(nil)
+	_ = []int(nil)
+	_ = map[int]bool(nil)
+	_ = chan string(nil)
+	_ = (func())(nil)
+	_ = interface{}(nil)
+
+
 }
---
+++ chapter_8_concurrency/06go_sleep.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func run(task_id, sleeptime int, ch chan string) {
+
+	time.Sleep(time.Duration(sleeptime) * time.Second)
+	ch <- fmt.Sprintf("task id %d , sleep %d second", task_id, sleeptime)
+	return
+}
+
+func main() {
+	input := []int{3, 2, 1}
+	ch := make(chan string)
+	startTime := time.Now()
+	fmt.Println("Multirun start")
+	for i, sleeptime := range input {
+		go run(i, sleeptime, ch)
+	}
+
+	for range input {
+		fmt.Println(<-ch)
+	}
+
+	endTime := time.Now()
+	fmt.Printf("Multissh finished. Process time %s. Number of tasks is %d", endTime.Sub(startTime), len(input))
+}
