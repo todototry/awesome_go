1089cd39ea9f8f4e162ebed194ac319568fe8f9c
todototry <todototry@gmail.com>
2018-11-20 16:42:29 +0800

    code: pointer convert from uintptr to unsafe.Pointer

--- chapter_15_memory/05_zero_sized_struct_transfer/main.go
+++ chapter_15_memory/05_zero_sized_struct_transfer/main.go
@@ -1,5 +1,23 @@
 package main
 
+
 func main() {
-	
+/*
+Types which zero values can be represented with nil
+The zero values of the following types can be represented with nil.
+
+Type (T)	Size Of T(nil)
+	pointer	1 word
+	slice	3 words
+	map	1 word
+	channel	1 word
+	function	1 word
+	interface	2 words
+(One word means 4 bytes on 32bits OSes and 8 bytes on 64bits OSes. and the indirect underlying parts of a value don't contribute to the size of the value.)
+
+The size of a zero value of a type is the same as any other values of the same type.
+ */
+
+
+
 }
