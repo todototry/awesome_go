a722a62154b5d84fcb3234f440cc342ea42b3e59
todototry <todototry@gmail.com>
2018-11-26 14:12:52 +0800

    refactor: dir names.

--- chapter_8_concurrency/10_common_concurrent_mistakes/01_receiver_copy_problems.go
+++ chapter_8_concurrency/10_common_concurrent_mistakes/01_receiver_copy_problems.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"sync"
 	"fmt"
+	"sync"
 )
 
 type Counter struct {
@@ -19,19 +19,20 @@ func (c *Counter) Increase(d int64) (r int64) {
 	return
 }
 
+// 此处会有问题，凡是有 sync 包的地方，都不能用 原始的struct receiver， 必须用指针
 // The method is bad. When it is called, a Counter
 // receiver value will be copied.
 func (c Counter) Value() (r int64) {
 	c.Lock()
 	c.n++
 	r = c.n
-	fmt.Printf("%P, %v %d \n", &c, &(c.n), c.n)  // address of c.n  here is different.
+	fmt.Printf("%P, %v %d \n", &c, &(c.n), c.n) // address of c.n  here is different.
 	c.Unlock()
 	return
 }
 
 func main() {
-	c := Counter{n:1}
+	c := Counter{n: 1}
 	fmt.Printf("%P, %v %d \n", &c, &(c.n), c.n)
 	c.Value()
 	fmt.Printf("%P, %v %d \n", &c, &(c.n), c.n)
